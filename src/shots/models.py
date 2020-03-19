77c855b86372523395ab87982c0d3462e47d9f41
Sam Texas <sam@simplecto.com>
2020-03-20 00:24:54 +0100

    remove cruft

--- src/shots/models.py
+++ src/shots/models.py
@@ -10,15 +10,6 @@ from django.core import validators
 from shots.validators import validate_hostname_dns
 
 
-def hash_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/{d1}/{d2}/{d3}/{d4}/<filename>
-    d1 = filename[0:2]
-    d2 = filename[2:4]
-    d3 = filename[4:6]
-    d4 = filename[6:8]
-
-    return f"{d1}/{d2}/{d3}/{d4}/{filename}"
-
 class ScreenShot(models.Model):
 
     NEW = 'N'
@@ -78,11 +69,6 @@ class ScreenShot(models.Model):
     @property
     def resolution(self):
         return f"{self.width}x{self.height}"
-
-    @property
-    def calculated_url(self):
-
-        return hash_directory_path(None, f"{self.id}.jpg")
 
     @property
     def full_data_uri(self):
